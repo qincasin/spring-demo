7eb8cb04cd6ae07dcd2497701ce4e26a835a3361
casin <qincasin@163.com>
2020-04-09 10:00:55 +0800

    fix: for aop

--- spring-annotation/src/main/java/com/qjx/config/MainConfigOfAOP.java
+++ spring-annotation/src/main/java/com/qjx/config/MainConfigOfAOP.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
+ * <pre>
  * AOP: 【动态代理】
  * 指在程序运行期间动态的将某段代码切入到指定方法指定位置进行运行的编程方式
  * 1.导入aop模块：spring aop
@@ -28,18 +29,42 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *      2. 在切面类上的每一个通知方法上标注通知注解，告诉spring何时何地运行(切入点表达式)
  *      3. 开启基于注解的aop模式：@EnableAspectJAutoProxy
  *
+ * AOP原理：【看给容器中注册了什么组件，这个组件什么时候工作，这个组件的功能是什么？】
+ * 1. @EnableAspectJAutoProxy是什么？
+ *      @Import(AspectJAutoProxyRegistrar.class) ：给容器中导入AspectJAutoProxyRegistrar
+ *            利用AspectJAutoProxyRegistrar 自定义给容器中注册bean
+ *            !! 注册一个组件 internalAutoProxyCreator ，这个组件的类型是AnnotationAwareAspectJAutoProxyCreator
+ *          org.springframework.aop.config.internalAutoProxyCreator=AnnotationAwareAspectJAutoProxyCreator
+ *       给容器中注册一个AnnotationAwareAspectJAutoProxyCreator；
+ * 2.AnnotationAwareAspectJAutoProxyCreator 继承关系
+ *     ->AspectJAwareAdvisorAutoProxyCreator
+ *      ->AbstractAdvisorAutoProxyCreator
+ *       ->!!AbstractAutoProxyCreator
+ *          implements SmartInstantiationAwareBeanPostProcessor, BeanFactoryAware
+ *        关注后置处理器(在bean初始化完成前后做事情)、自动装配 BeanFactory
+ *  由后往前分析：
+ *      AbstractAutoProxyCreator
+ *          AbstractAutoProxyCreator.setBeanFactory() 自动装配
+ *          AbstractAutoProxyCreator.postProcessAfter/Before XXX 有后置处理器的逻辑
+ *      AbstractAdvisorAutoProxyCreator
+ *          AbstractAdvisorAutoProxyCreator.setBeanFactory() 重写 ==》调用内部 initBeanFactory((ConfigurableListableBeanFactory) beanFactory);
+ *      AspectJAwareAdvisorAutoProxyCreator
+ *          无后置处理器 也 初始化相关操作
+ *      AnnotationAwareAspectJAutoProxyCreator
+ *          AnnotationAwareAspectJAutoProxyCreator.initBeanFactory
+ * </pre>
  */
 @EnableAspectJAutoProxy
 @Configuration
 public class MainConfigOfAOP {
 
     @Bean
-    public MathCalculate calculate(){
+    public MathCalculate calculate() {
         return new MathCalculate();
     }
 
     @Bean
-    public LogAspects logAspects(){
+    public LogAspects logAspects() {
         return new LogAspects();
     }
 
